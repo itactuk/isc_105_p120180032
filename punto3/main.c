08eee6428a4cab6d26f0149b7cbabc58d8ea0ff0
20180032 <54964850+20180032@users.noreply.github.com>
2019-10-15 21:26:20 -0400

    Add files via upload

---
+++ punto3/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define CANTIDAD_MAXIMA_RESULTADOS 100
+#define N 3
+#define M 4
+
+typedef struct indices{
+    int x;
+    int y;
+} INDICES;
+
+typedef struct resultados{
+    INDICES indices[CANTIDAD_MAXIMA_RESULTADOS];
+    int cantidad_resultados_indices;
+} RESULTADOS;
+
+int puntos_de_silla(int mat[N][M], int posFila, int posColumna);
+
+int main () {
+float matriz[2][2] = {1,2,3,4};
+return 0;
+}
+
+int  puntos_de_silla(int mat[N][M], int posFila, int posColumna) {    //funcion bien
+    int i, j, enc=0;
+
+    for(i=0; i<N && !enc; i++)
+    {
+        for(j=0; j<M && !enc; j++)
+        {
+            if(mat[posColumna][posFila]>mat[posFila][j] && mat[posColumna][posFila]<mat[i][posColumna])
+                enc =1;
+        }
+    }
+
+    return enc;
+}
+
+
+
